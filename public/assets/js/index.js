6db02dfcfad212233d77a87a9f190b532e8f5ae2
Erwann <erwann.leroux1@gmail.com>
2021-01-26 14:07:38 +0100

    canvas allow to move emitters

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,21 +1,25 @@
 import { Canvas } from './modules/canvas.js';
-import { ReasonnanceController } from './modules/reasonnance.js'
+import { ResonanceController } from './modules/reasonnance.js'
 
 document.addEventListener("DOMContentLoaded", function() {
   let canvas                = document.querySelector("#scene-canvas")
   let playButton            = document.querySelector("#play-btn")
   let elements              = [
-      {x: 0.5, y: 0.5, path: "coeur"},
-      {x: 0.75, y: 0, path: "coeur"},
-      {x: 0, y: 0.75, path: "coeur"}
+      {x: 0.5, y: 0.5, path: "coeur",radius: 0.04, alpha: 0.75, clickable: true, type: "listener",
+          icon: "listener.svg"},
+      {x: 0.25, y: 0.5, path: "music.wav",radius: 0.04, alpha: 0.75, clickable: true, type: "emitter",
+          icon : "emitter.svg"},
+      {x: 0.75, y: 0.5, path: "coeur.mp3",radius: 0.04, alpha: 0.75, clickable: true, type: "emitter",
+          icon : "emitter.svg"},
   ]
   let ctx                   = canvas.getContext('2d')
-  let canvasController      = new Canvas(canvas, elements)
-  let reasonnanceController = null
+  let canvasController      = null
+  let resonanceController = null
 
   playButton.addEventListener('click', function(e) {
       /* Chrome doesn't allow audio context creation until user action */
-      if(!reasonnanceController) reasonnanceController =  new ReasonnanceController(elements)
-      reasonnanceController.playAll()
+      if(!resonanceController) resonanceController =  new ResonanceController(elements)
+      if(!canvasController) canvasController = new Canvas(canvas, elements, resonanceController)
+      resonanceController.playAll()
   })
 });
