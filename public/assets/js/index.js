0408d795c024d0d77f19cf1238033c136d0aac6e
Antoine Gonzalez <antoine.gonzalez61@live.fr>
2021-01-26 15:42:26 +0100

    upload de sources audios

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
   let canvas                = document.querySelector("#scene-canvas")
   let playButton            = document.querySelector("#play-btn")
   let elements              = [
-      {x: 0.5, y: 0.5, path: "coeur"},
+      {x: 0.5, y: 0.5, path: "clavier"},
       {x: 0.75, y: 0, path: "coeur"},
-      {x: 0, y: 0.75, path: "coeur"}
+      {x: 0, y: 0.75, path: "foret"}
+  
   ]
   let ctx                   = canvas.getContext('2d')
   let canvasController      = new Canvas(canvas, elements)
@@ -18,4 +19,53 @@ document.addEventListener("DOMContentLoaded", function() {
       if(!reasonnanceController) reasonnanceController =  new ReasonnanceController(elements)
       reasonnanceController.playAll()
   })
+
+  // UPLOAD 
+  let uploadController = document.querySelector("#upload-controller")
+  uploadController.addEventListener("click", (e) =>{
+    e.preventDefault()
+    let uploadForm = document.querySelector("#upload-form")
+    if(uploadForm.classList.contains("hidden")){
+      uploadForm.classList.remove("hidden")
+    }else{
+      uploadForm.classList.add("hidden")
+    }
+    
+
+  })
+
+  let sounds = document.querySelector("#sounds-upload")
+  let form = document.querySelector("#form-upload")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let soundfiles = sounds.files
+
+    let formData = new FormData()
+    Array.from(soundfiles).forEach(soundfile => {
+      formData.append('sounds', soundfile)
+    }) 
+
+    formData.append('sounds', soundfiles)
+    let xhr = new XMLHttpRequest()
+    xhr.responseType = 'json'
+    xhr.onload = () => {
+      if(xhr.response.status == "success"){
+        actualiseSoundsList(xhr.response.data)
+      }
+    }
+    xhr.open('POST', '/upload');
+    xhr.send(formData);
+  })
+
 });
+
+function actualiseSoundsList(sounds){
+  let soundBag = document.querySelector("#sounds-container")
+  sounds.forEach((sound) => {
+    let element = document.createElement('div')
+    element.classList.add('sound-item')
+    let elementText = document.createTextNode(sound.name)
+    element.appendChild(elementText)
+    soundBag.appendChild(element)
+  })
+}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
 const fs         = require('fs')
 const express    = require('express')
 const bodyParser = require('body-parser')
 const cors       = require('cors')
 const path       = require('path')
+const fileUpload = require('express-fileupload');
 
 
 async function main() {
@@ -17,13 +17,88 @@ async function main() {
     app.use('/assets', express.static('public/assets'))
     app.use('/resources', express.static('resources'))
     app.use('/resonance', express.static('node_modules/resonance-audio'))
+    app.use(fileUpload());
+
 
     router.get('/', (request, response) => {
-        response.render("index")
+        let directoryPath = path.join(__dirname, '\\resources\\audio')
+        readDirectory(directoryPath).then((soundsList) => {
+            response.render("index", {"sounds": soundsList})
+        }).catch((err)=> {
+            response.status(500).json({
+                error: err
+            })
+        })
     });
+
+    router.post('/upload', function(req, res) {
+        try {
+            if (!req.files) {
+                res.send({
+                    status: false,
+                    message: 'No file uploadedSounds'
+                });
+            } else {
+                let uploadedSounds = req.files.sounds;
+                if(!(uploadedSounds instanceof Array)){
+                    uploadedSounds = [req.files.sounds]
+                }
+                let uploadDirectoryPath = __dirname + '\\resources\\audio\\'   
+                readDirectory(uploadDirectoryPath).then((soundsList) => {
+                    let promises = []
+                    for(let i = 0; i<uploadedSounds.length; i++){
+                        let initialName = uploadedSounds[i].name.split('.')
+                        let extension = initialName.pop()
+
+                        let id = 1
+                        
+                        while(soundsList.includes(uploadedSounds[i].name)){
+                            uploadedSounds[i].name = initialName +` - (${id}).` + extension
+                            id = id + 1
+                        }
+                        promises.push(uploadedSounds[i].mv(uploadDirectoryPath + uploadedSounds[i].name))
+                    }
+                    Promise.all(promises)
+                    .then(() => {
+                        let soundAdded = []
+                        uploadedSounds.forEach((sound) => {
+                            soundAdded.push({
+                                name : sound.name,
+                            })
+                        })
+                        res.status(200).json({
+                            status: 'success',
+                            data : soundAdded
+                        })
+                    }).catch((err) => {
+                        res.status(400).json({
+                            status: 'failure',
+                            error: err
+                        })
+                    })
+                })
+            }
+        }catch (err){
+            res.status(500).json({
+                status: 'failure',
+                error: err
+            })
+        }
+    })
 
     app.use('/', router)
     app.listen(8080)
+}
+
+function readDirectory(directoryPath){
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, function(err, soundsList){
+            if(err){
+                reject(err)
+            }
+            resolve(soundsList)
+        })
+    })
 }
 
 main()
