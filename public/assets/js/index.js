ba4718b5d47062712537104be4f975cea3d00263
Erwann <erwann.leroux1@gmail.com>
2021-01-28 15:49:29 +0100

    bug fixed when adding sound to dom

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,85 +4,88 @@ import { ResonanceController } from './modules/reasonnance.js'
 document.addEventListener("DOMContentLoaded", function() {
   let canvas                = document.querySelector("#scene-canvas")
   let playButton            = document.querySelector("#play-btn")
-  let elements              = [
+  let soundTable            = document.querySelector(".sound-table")
+  let soundsItem            = document.querySelectorAll(".sound-item")
+  let canvasContainer       = document.querySelector("#scene-canvas-container")
+
+  /* Upload relative fields */
+  let uploadCollapse         = document.querySelector("#upload-controller")
+  let uploadForm             = document.querySelector("#upload-form")
+  let uploadInput            = document.querySelector("#sounds-upload__input")
+  let uploadButton           = document.querySelector("#sounds-upload__button")
+  let uploadLabel            = document.querySelector("#sounds-upload__label")
+
+  let playAllButton          = document.querySelector(".play-all")
+
+  let defaultLabelText       = "Aucun fichier sélectionné"
+  let elements               = [
       {x: 0.5, y: 0.5, path: "",radius: 0.04, alpha: 0.75, clickable: true, type: "listener",
           icon: "listener.svg"}
   ]
-
   let ctx                   = canvas.getContext('2d')
   let canvasController      = null
   let resonanceController   = null
+  let selectedElement       = null
 
-  let soundTable       = document.querySelector(".sound-table")
-  let soundsItem       = document.querySelectorAll(".sound-item")
-  let canvasContainer  = document.querySelector("#scene-canvas-container")
-
-  let selectedElement  = null
-
+  uploadLabel.textContent = defaultLabelText;
+  uploadLabel.title       = defaultLabelText;
   canvas.width  = canvasContainer.clientWidth
   canvas.height = canvasContainer.clientHeight
-
-  canvas.addEventListener('drop', (e) => {
-    e.preventDefault()
-
-    const rect = canvas.getBoundingClientRect()
-    const x = (e.clientX - rect.left) / canvas.width
-    const y = (e.clientY - rect.top) / canvas.height
-
-    /* Generating new element then add it to the audio scene */
-    let el = { x: x, y: y, radius: 0.04, alpha: 0.75, playing: true, clickable: true, type: "emitter",
-      icon: `emitters/emitter${elements.length}.svg`, path: selectedElement.path, ext: selectedElement.ext}
-
-    /* remove element from the list */
-    let droppedElement = document.querySelector(`.sound-item[data-path='${selectedElement.path}']`)
-    droppedElement.remove()
-
-    canvasController.addElement(el)
-
-    addSoundDOM(el, soundTable, canvasController, resonanceController)
-  })
-
-  canvas.addEventListener('dragover', (e) => {
-    e.preventDefault()
-  })
-
-  soundsItem.forEach((item) => {
-    item.addEventListener('dragstart', (e) => {
-      selectedElement = {
-        path: item.dataset.path,
-        ext: item.dataset.ext
-      }
-    })
-  })
 
   playButton.addEventListener('click', function(e) {
     /* Chrome doesn't allow audio context creation until user action */
     if(!resonanceController) resonanceController =  new ResonanceController(elements)
     if(!canvasController) canvasController = new Canvas(canvas, elements, resonanceController)
     resonanceController.playAll()
-  })
 
-  /* Upload */
-  let uploadCollapse         = document.querySelector("#upload-controller")
-  let uploadForm             = document.querySelector("#upload-form")
-  let uploadInput            = document.querySelector("#sounds-upload__input")
-  let uploadButton           = document.querySelector("#sounds-upload__button")
-  let uploadLabel            = document.querySelector("#sounds-upload__label")
-  let playAllButton          = document.querySelector(".play-all")
-  let defaultLabelText       = "Aucun fichier sélectionné"
+    canvas.addEventListener('drop', (e) => {
+      e.preventDefault()
 
-  uploadLabel.textContent = defaultLabelText;
-  uploadLabel.title       = defaultLabelText;
+      if(selectedElement != null) {
+        const rect = canvas.getBoundingClientRect()
+        const x = (e.clientX - rect.left) / canvas.width
+        const y = (e.clientY - rect.top) / canvas.height
 
-  playAllButton.addEventListener('click', (e) => {
-    /* retrieve all buttons and pass it to the functions */
-    let allButtons = soundTable.querySelectorAll('.single-control-button')
+        /* Generating new element then add it to the audio scene */
+        let el = { x: x, y: y, radius: 0.04, alpha: 0.75, playing: true, clickable: true, type: "emitter",
+          icon: `emitters/emitter${elements.length}.svg`, path: selectedElement.path, ext: selectedElement.ext}
 
-    if(playAllButton.textContent == 'Stop All') {
-      stopAll(resonanceController, playAllButton, allButtons)
-    } else {
-      playAll(resonanceController, playAllButton, allButtons)
-    }
+        /* remove element from the list */
+        let droppedElement = document.querySelector(`.sound-item[data-path='${selectedElement.path}']`)
+        droppedElement.style.display = 'none'
+
+        canvasController.addElement(el)
+
+        addSoundDOM(el, soundTable, canvasController, resonanceController)
+        /* reset selected element to null */
+        selectedElement = null
+      }
+    })
+
+    canvas.addEventListener('dragover', (e) => {
+      e.preventDefault()
+    })
+
+    soundsItem.forEach((item) => {
+      item.addEventListener('dragstart', (e) => {
+        selectedElement = {
+          path: item.dataset.path,
+          ext: item.dataset.ext
+        }
+      })
+    })
+
+    playAllButton.addEventListener('click', (e) => {
+      /* retrieve all buttons and pass it to the functions */
+      let allButtons = soundTable.querySelectorAll('.single-control-button')
+
+      if(playAllButton.textContent == 'Stop All') {
+        stopAll(resonanceController, playAllButton, allButtons)
+      } else {
+        playAll(resonanceController, playAllButton, allButtons)
+      }
+    })
+
   })
 
   uploadButton.addEventListener('click', (e) => {
@@ -134,18 +137,18 @@ document.addEventListener("DOMContentLoaded", function() {
     sounds.forEach((sound) => {
       let element     = document.createElement('div')
       let elementText = document.createTextNode(sound.name)
+      let arr         = sound.name.split('.')
+
+      element.dataset.path = arr[0]
+      element.dataset.ext  = arr[1]
+      element.draggable    = true
 
       element.classList.add('sound-item')
 
-
-      element.dataset.path = sound.name
-
       element.addEventListener('dragstart', (e) => {
-        selectedElement.path = element.dataset.path
-        selectedElement.ext  = element.dataset.ext
+        selectedElement = { path: element.dataset.path, ext: element.dataset.ext}
       })
 
-      element.draggable = true
       element.appendChild(elementText)
       soundBag.appendChild(element)
     })
@@ -164,6 +167,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     deleteIc.addEventListener('click', (e) => {
       canvasController.removeElement(element)
+
+      let DOMElement = document.querySelector(`.sound-item[data-path='${element.path}']`)
+      DOMElement.style.display = 'revert'
+
       tr.remove()
       /* display empty message if no more data */
       if(tbody.querySelectorAll('tr').length == 2) {
